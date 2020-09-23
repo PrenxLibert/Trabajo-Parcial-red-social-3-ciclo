a436c54e5eb18f8019b037d69d845e131972a7ee
LeoGrau <54156661+LeoGrau@users.noreply.github.com>
2020-09-23 16:23:30 -0500

    Progreso_1
    Es una avance de prueba

--- Red_Social/baseredsocial.h
+++ Red_Social/baseredsocial.h
@@ -2,7 +2,7 @@
 #define BASEREDSOCIAL_H
 
 #include <QMainWindow>
-
+#include "usuario.h"
 QT_BEGIN_NAMESPACE
 namespace Ui { class baseredsocial; }
 QT_END_NAMESPACE
@@ -11,9 +11,13 @@ class baseredsocial : public QMainWindow
 {
     Q_OBJECT
 
+
 public:
     baseredsocial(QWidget *parent = nullptr);
     ~baseredsocial();
+
+private slots:
+    void on_pushButton_clicked();
 
 private:
     Ui::baseredsocial *ui;
