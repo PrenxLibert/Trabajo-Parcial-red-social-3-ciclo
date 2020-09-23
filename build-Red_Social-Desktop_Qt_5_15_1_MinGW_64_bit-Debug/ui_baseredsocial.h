c288d71e2ce5ed9ffc2eaf15b3a0f86c9fd9d791
kevinstif <kevin_stif_2012@hotmail.com>
2020-09-23 09:18:26 -0500

    avance 1

---
+++ build-Red_Social-Desktop_Qt_5_15_1_MinGW_64_bit-Debug/ui_baseredsocial.h
@@ -0,0 +1,168 @@
+/********************************************************************************
+** Form generated from reading UI file 'baseredsocial.ui'
+**
+** Created by: Qt User Interface Compiler version 5.15.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_BASEREDSOCIAL_H
+#define UI_BASEREDSOCIAL_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QGroupBox>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_baseredsocial
+{
+public:
+    QWidget *centralwidget;
+    QLabel *label;
+    QLineEdit *lineEdit;
+    QLabel *label_2;
+    QLineEdit *lineEdit_2;
+    QPushButton *pushButton;
+    QGroupBox *groupBox;
+    QLabel *label_3;
+    QLabel *label_4;
+    QLineEdit *lineEdit_3;
+    QLabel *label_5;
+    QLineEdit *lineEdit_4;
+    QLabel *label_6;
+    QLineEdit *lineEdit_5;
+    QLabel *label_7;
+    QLabel *label_8;
+    QLabel *label_9;
+    QLineEdit *lineEdit_6;
+    QLineEdit *lineEdit_7;
+    QLineEdit *lineEdit_8;
+    QPushButton *pushButton_2;
+    QLabel *label_10;
+    QGroupBox *groupBox_2;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *baseredsocial)
+    {
+        if (baseredsocial->objectName().isEmpty())
+            baseredsocial->setObjectName(QString::fromUtf8("baseredsocial"));
+        baseredsocial->resize(800, 534);
+        centralwidget = new QWidget(baseredsocial);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        label = new QLabel(centralwidget);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setGeometry(QRect(310, 20, 51, 21));
+        lineEdit = new QLineEdit(centralwidget);
+        lineEdit->setObjectName(QString::fromUtf8("lineEdit"));
+        lineEdit->setGeometry(QRect(350, 20, 113, 20));
+        label_2 = new QLabel(centralwidget);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+        label_2->setGeometry(QRect(480, 20, 61, 16));
+        lineEdit_2 = new QLineEdit(centralwidget);
+        lineEdit_2->setObjectName(QString::fromUtf8("lineEdit_2"));
+        lineEdit_2->setGeometry(QRect(550, 20, 113, 20));
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setGeometry(QRect(680, 20, 75, 23));
+        groupBox = new QGroupBox(centralwidget);
+        groupBox->setObjectName(QString::fromUtf8("groupBox"));
+        groupBox->setGeometry(QRect(290, 10, 481, 41));
+        label_3 = new QLabel(centralwidget);
+        label_3->setObjectName(QString::fromUtf8("label_3"));
+        label_3->setGeometry(QRect(310, 100, 61, 21));
+        label_4 = new QLabel(centralwidget);
+        label_4->setObjectName(QString::fromUtf8("label_4"));
+        label_4->setGeometry(QRect(330, 150, 47, 13));
+        lineEdit_3 = new QLineEdit(centralwidget);
+        lineEdit_3->setObjectName(QString::fromUtf8("lineEdit_3"));
+        lineEdit_3->setGeometry(QRect(390, 150, 381, 20));
+        label_5 = new QLabel(centralwidget);
+        label_5->setObjectName(QString::fromUtf8("label_5"));
+        label_5->setGeometry(QRect(330, 190, 51, 21));
+        lineEdit_4 = new QLineEdit(centralwidget);
+        lineEdit_4->setObjectName(QString::fromUtf8("lineEdit_4"));
+        lineEdit_4->setGeometry(QRect(390, 190, 381, 20));
+        label_6 = new QLabel(centralwidget);
+        label_6->setObjectName(QString::fromUtf8("label_6"));
+        label_6->setGeometry(QRect(330, 230, 47, 21));
+        lineEdit_5 = new QLineEdit(centralwidget);
+        lineEdit_5->setObjectName(QString::fromUtf8("lineEdit_5"));
+        lineEdit_5->setGeometry(QRect(390, 230, 381, 20));
+        label_7 = new QLabel(centralwidget);
+        label_7->setObjectName(QString::fromUtf8("label_7"));
+        label_7->setGeometry(QRect(280, 270, 101, 21));
+        label_8 = new QLabel(centralwidget);
+        label_8->setObjectName(QString::fromUtf8("label_8"));
+        label_8->setGeometry(QRect(310, 310, 71, 20));
+        label_9 = new QLabel(centralwidget);
+        label_9->setObjectName(QString::fromUtf8("label_9"));
+        label_9->setGeometry(QRect(310, 350, 61, 20));
+        lineEdit_6 = new QLineEdit(centralwidget);
+        lineEdit_6->setObjectName(QString::fromUtf8("lineEdit_6"));
+        lineEdit_6->setGeometry(QRect(390, 270, 381, 20));
+        lineEdit_7 = new QLineEdit(centralwidget);
+        lineEdit_7->setObjectName(QString::fromUtf8("lineEdit_7"));
+        lineEdit_7->setGeometry(QRect(390, 310, 381, 20));
+        lineEdit_8 = new QLineEdit(centralwidget);
+        lineEdit_8->setObjectName(QString::fromUtf8("lineEdit_8"));
+        lineEdit_8->setGeometry(QRect(390, 350, 381, 20));
+        pushButton_2 = new QPushButton(centralwidget);
+        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+        pushButton_2->setGeometry(QRect(390, 410, 75, 23));
+        label_10 = new QLabel(centralwidget);
+        label_10->setObjectName(QString::fromUtf8("label_10"));
+        label_10->setGeometry(QRect(310, 410, 71, 16));
+        groupBox_2 = new QGroupBox(centralwidget);
+        groupBox_2->setObjectName(QString::fromUtf8("groupBox_2"));
+        groupBox_2->setGeometry(QRect(270, 130, 511, 341));
+        baseredsocial->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(baseredsocial);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 800, 21));
+        baseredsocial->setMenuBar(menubar);
+        statusbar = new QStatusBar(baseredsocial);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        baseredsocial->setStatusBar(statusbar);
+
+        retranslateUi(baseredsocial);
+
+        QMetaObject::connectSlotsByName(baseredsocial);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *baseredsocial)
+    {
+        baseredsocial->setWindowTitle(QCoreApplication::translate("baseredsocial", "baseredsocial", nullptr));
+        label->setText(QCoreApplication::translate("baseredsocial", "Usuario", nullptr));
+        label_2->setText(QCoreApplication::translate("baseredsocial", "Contrase\303\261a", nullptr));
+        pushButton->setText(QCoreApplication::translate("baseredsocial", "Log in", nullptr));
+        groupBox->setTitle(QString());
+        label_3->setText(QCoreApplication::translate("baseredsocial", "Resgitrarse", nullptr));
+        label_4->setText(QCoreApplication::translate("baseredsocial", "Nombre :", nullptr));
+        label_5->setText(QCoreApplication::translate("baseredsocial", "Apellidos :", nullptr));
+        label_6->setText(QCoreApplication::translate("baseredsocial", "Telefono :", nullptr));
+        label_7->setText(QCoreApplication::translate("baseredsocial", "Nombre de usuario :", nullptr));
+        label_8->setText(QCoreApplication::translate("baseredsocial", "Contrase\303\261a :", nullptr));
+        label_9->setText(QCoreApplication::translate("baseredsocial", "Contrace\303\261a :", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("baseredsocial", "Registrar", nullptr));
+        label_10->setText(QCoreApplication::translate("baseredsocial", "Resgistrarse :", nullptr));
+        groupBox_2->setTitle(QString());
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class baseredsocial: public Ui_baseredsocial {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_BASEREDSOCIAL_H
